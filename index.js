752b481c923d15363aa85b8b0c84847aa4a80bff
ikealmighty <isaacyates7@gmail.com>
2023-10-11 15:24:12 -0500

    wrote (but didn't test) /endturn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fastify = Fastify({
     logger: true
 })
 
-const { MongoClient } = require('mongodb');
 fastify.register(require('fastify-mongodb'), { url: process.env.MONGO_URI })
 
 import jwt from 'jsonwebtoken';
@@ -43,7 +42,7 @@ fastify.get('/join', async (req, res) => {
             let newGameDocument = {
                 players: [{ username }],
                 gameStarted: false,
-                activePlayer: null,
+                activePlayerName: null,
                 territories,
             }
 
@@ -102,7 +101,7 @@ fastify.get('/attack', async (req, res) => {
 
         // if the request did not come from an active player, 
         // then return an error:
-        if (gamestate.activePlayer !== decoded.username) {
+        if (gamestate.activePlayerName !== decoded.username) {
             res.send({ error: `${decoded.username} is not the active player.` })
         }
 
@@ -156,9 +155,43 @@ fastify.get('/attack', async (req, res) => {
     }
 })
 
-fastify.get('/endturn', (req, res) => {
+fastify.get('/endturn', async (req, res) => {
     // set the game state to reflect the next player in
     // turn order to be the active player.
+
+    const token = req.headers.authorization.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+        const db = fastify.mongo.db;
+        const gamestate = await db.collection('games').findOne({ _id: decoded.gameId });
+
+        // don't allow anybody but the active player to end the turn:
+        if (decoded.username !== gamestate.activePlayerName) {
+            res.send({ error: `only the currently active player, ${gamestate.activePlayerName}, can end the turn` })
+            return;
+        }
+
+        // find the index of the active player based on username
+        let activePlayerIndex = gamestate.players.reduce((prev, curr, index) => gamestate.activePlayerName === curr.username ? index : null);
+
+        // provided there is an active player, update game state to show the next player
+        // in the turn loop to be the activePlayerName:
+        if (activePlayerIndex === null) {
+            res.send({ error: 'no active player' })
+            return;
+        }
+        else if (activePlayerIndex === gamestate.players.length - 1) activePlayerIndex = 0;
+        else activePlayerIndex += 1;
+
+        gamestate.activePlayerName = gamestate.players[activePlayerIndex].username;
+
+        // update the database to reflect active player name:
+        const mongoRes = await db.collection('games').updateOne({ _id: decoded.gameId }, { $set: { activePlayerName: gamestate.activePlayerName } })
+        if (mongoRes.matchedCount === 0) throw new Error(`unable to find match on gameId ${decoded.gameId}`)
+        res.send({ gamestate })
+    } catch (err) {
+        fastify.log.error(err);
+    }
 })
 
 fastify.post('/reinforce', (req, res) => {
@@ -179,10 +212,10 @@ fastify.get('/gamestate', (req, res) => {
     /**
      * Game State Model:
      * {
-     *      activePlayer: [playerId | None]
+     *      activePlayerName: [string | None]
      *      territories: {... all the territories in the game}
      *      timeSinceLastTurn: [milliseconds]
-     *      winner: [playerId | None]
+     *      winnerName: [string | None]
      * }
      */
 })
